8f87073488073630e53b8f65d1076971f681d628
parfaithery771-crypto <parfaithery771@gmail.com>
2026-06-18 14:55:00 +0300

    isolate include test

--- webroot/dbtest.php
+++ webroot/dbtest.php
@@ -1,5 +1,7 @@
 <?php
 header("Content-Type: text/plain");
-$content = file_get_contents(dirname(__DIR__) . "/config/app_local.php");
-$content = preg_replace("/(password[\"\x27]\\s*=>\\s*[\"\x27])[^\"\x27]*([\"\x27])/i", "$1HIDDEN$2", $content);
-echo $content;
+require dirname(__DIR__) . "/vendor/autoload.php";
+$result = include dirname(__DIR__) . "/config/app_local.php";
+echo "debug from direct include = " . var_export($result["debug"] ?? "KEY MISSING", true) . "\n";
+echo "Datasources.default.url = " . var_export($result["Datasources"]["default"]["url"] ?? "KEY MISSING", true) . "\n";
+echo "raw getenv DEBUG here = " . var_export(getenv("DEBUG"), true) . "\n";
