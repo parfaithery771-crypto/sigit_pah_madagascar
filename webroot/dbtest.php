c8418d5bc4d4d025cffed032ebf46f186c790b3c
parfaithery771-crypto <parfaithery771@gmail.com>
2026-06-18 14:52:10 +0300

    show real app_local.php content

--- webroot/dbtest.php
+++ webroot/dbtest.php
@@ -1,14 +1,5 @@
 <?php
 header("Content-Type: text/plain");
-require dirname(__DIR__) . "/vendor/autoload.php";
-use App\Application;
-$app = new Application(dirname(__DIR__) . "/config");
-
-echo "file_exists app_local.php = " . var_export(file_exists(dirname(__DIR__) . "/config/app_local.php"), true) . "\n";
-echo "raw getenv DEBUG = " . var_export(getenv("DEBUG"), true) . "\n";
-echo "filter_var manual = " . var_export(filter_var(getenv("DEBUG"), FILTER_VALIDATE_BOOLEAN), true) . "\n";
-echo "Configure debug = " . var_export(\Cake\Core\Configure::read("debug"), true) . "\n";
-
-$conn = \Cake\Datasource\ConnectionManager::getConfig("default");
-if (isset($conn["password"])) { $conn["password"] = "HIDDEN"; }
-echo "ConnectionManager config default:\n" . print_r($conn, true) . "\n";
+$content = file_get_contents(dirname(__DIR__) . "/config/app_local.php");
+$content = preg_replace("/(password[\"\x27]\\s*=>\\s*[\"\x27])[^\"\x27]*([\"\x27])/i", "$1HIDDEN$2", $content);
+echo $content;
