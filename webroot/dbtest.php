f679a56ff6b67275fdc9892ff75ae3becab029b0
parfaithery771-crypto <parfaithery771@gmail.com>
2026-06-18 14:47:42 +0300

    deeper diagnose

--- webroot/dbtest.php
+++ webroot/dbtest.php
@@ -3,9 +3,12 @@ header("Content-Type: text/plain");
 require dirname(__DIR__) . "/vendor/autoload.php";
 use App\Application;
 $app = new Application(dirname(__DIR__) . "/config");
-echo "debug = " . var_export(\Cake\Core\Configure::read("debug"), true) . "\n";
-$ds = \Cake\Core\Configure::read("Datasources.default");
-if (isset($ds["password"])) { $ds["password"] = "HIDDEN"; }
-echo "Datasources.default:\n" . print_r($ds, true) . "\n";
-echo "getenv DATABASE_URL = " . (getenv("DATABASE_URL") ? "SET" : "NOT SET") . "\n";
-echo "getenv DEBUG = " . var_export(getenv("DEBUG"), true) . "\n";
+
+echo "file_exists app_local.php = " . var_export(file_exists(dirname(__DIR__) . "/config/app_local.php"), true) . "\n";
+echo "raw getenv DEBUG = " . var_export(getenv("DEBUG"), true) . "\n";
+echo "filter_var manual = " . var_export(filter_var(getenv("DEBUG"), FILTER_VALIDATE_BOOLEAN), true) . "\n";
+echo "Configure debug = " . var_export(\Cake\Core\Configure::read("debug"), true) . "\n";
+
+$conn = \Cake\Datasource\ConnectionManager::getConfig("default");
+if (isset($conn["password"])) { $conn["password"] = "HIDDEN"; }
+echo "ConnectionManager config default:\n" . print_r($conn, true) . "\n";
