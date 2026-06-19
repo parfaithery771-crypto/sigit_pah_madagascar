816dce1334bae5deb342371f9d1ac06256c8e51e
parfaithery771-crypto <parfaithery771@gmail.com>
2026-06-19 14:04:04 +0300

    force display php errors

--- webroot/index.php
+++ webroot/index.php
@@ -1,4 +1,10 @@
 <?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+/**
+ * The Front Controller for handling every request
 /**
  * The Front Controller for handling every request
  *
