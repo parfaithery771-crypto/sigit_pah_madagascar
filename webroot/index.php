b6abe3f1f3f725af524a81320ee8cbdec2f85d61
parfaithery771-crypto <parfaithery771@gmail.com>
2026-06-19 15:15:26 +0300

    cleanup and restore error templates

--- webroot/index.php
+++ webroot/index.php
@@ -1,19 +1,14 @@
 <?php
-ini_set('display_errors', '1');
-error_reporting(E_ALL);
-
+if (PHP_SAPI === 'cli-server') {
+    $_SERVER['PHP_SELF'] = '/' . basename(__FILE__);
+    $url = parse_url(urldecode($_SERVER['REQUEST_URI']));
+    $file = __DIR__ . $url['path'];
+    if (!str_contains($url['path'], '..') && str_contains($url['path'], '.') && is_file($file)) {
+        return false;
+    }
+}
 require dirname(__DIR__) . '/vendor/autoload.php';
-
 use App\Application;
 use Cake\Http\Server;
-
-try {
-    $server = new Server(new Application(dirname(__DIR__) . '/config'));
-    $server->emit($server->run());
-} catch (\Throwable $e) {
-    header('Content-Type: text/plain');
-    echo "EXCEPTION CLASS: " . get_class($e) . "\n\n";
-    echo "MESSAGE: " . $e->getMessage() . "\n\n";
-    echo "FILE: " . $e->getFile() . " LINE: " . $e->getLine() . "\n\n";
-    echo "TRACE:\n" . $e->getTraceAsString();
-}
+$server = new Server(new Application(dirname(__DIR__) . '/config'));
+$server->emit($server->run());
