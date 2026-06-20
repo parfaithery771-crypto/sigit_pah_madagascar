fe44ea8cae9de3d2544825e05607ea23c2be749e
parfaithery771-crypto <parfaithery771@gmail.com>
2026-06-20 12:16:07 +0300

    remove error500 bak

--- templates/Error/error500_bak.php
+++
@@ -1,9 +0,0 @@
-<?php header('Content-Type: text/html; charset=utf-8'); ?>
-<!DOCTYPE html>
-<html lang="fr">
-<head><meta charset="UTF-8"><title>Erreur 500</title></head>
-<body>
-<h1>Erreur interne</h1>
-<p>Une erreur est survenue. Veuillez reessayer.</p>
-<a href="/">Retour a l'accueil</a>
-</body></html>
