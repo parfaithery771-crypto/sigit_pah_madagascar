c148aadafbf259f85d7d569f8dc04121a5317703
parfaithery771-crypto <parfaithery771@gmail.com>
2026-06-22 13:59:21 +0300

    debug users table

--- webroot/test_add.php
+++ webroot/test_add.php
@@ -1,5 +1,5 @@
 <?php
 $dsn = 'mysql:host=sql7.freesqldatabase.com;dbname=sql7830743;port=3306;charset=utf8mb4';
 $pdo = new PDO($dsn, 'sql7830743', 'X9UVEJYT3f');
-$stmt = $pdo->query('SELECT id, nom, email, role FROM users');
-while($r = $stmt->fetch()) echo $r['id'].' | '.$r['nom'].' | '.$r['email'].' | '.$r['role'].'<br>';
+$stmt = $pdo->query('DESCRIBE users');
+while($r = $stmt->fetch()) echo $r['Field'].' - '.$r['Type'].'<br>';
