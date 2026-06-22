8c833c6d49e5954428caa53196b00a6e3f83d3b1
parfaithery771-crypto <parfaithery771@gmail.com>
2026-06-22 14:21:57 +0300

    debug session

--- webroot/test_add.php
+++ webroot/test_add.php
@@ -1,5 +1,5 @@
 <?php
-$dsn = 'mysql:host=sql7.freesqldatabase.com;dbname=sql7830743;port=3306;charset=utf8mb4';
-$pdo = new PDO($dsn, 'sql7830743', 'X9UVEJYT3f');
-$stmt = $pdo->query('DESCRIBE users');
-while($r = $stmt->fetch()) echo $r['Field'].' - '.$r['Type'].'<br>';
+session_start();
+echo 'Session: <pre>';
+print_r($_SESSION);
+echo '</pre>';
