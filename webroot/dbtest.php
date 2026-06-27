4b9085039aa7f9eb5cdf8e21c6c858420df160bf
parfaithery771-crypto <parfaithery771@gmail.com>
2026-06-27 09:40:21 +0300

    check columns

---
+++ webroot/dbtest.php
@@ -0,0 +1,5 @@
+﻿<?php
+$dsn = 'mysql:host=mysql-22e1d2da-sigitmincc.i.aivencloud.com;port=14995;dbname=defaultdb;charset=utf8mb4';
+$pdo = new PDO($dsn, 'avnadmin', 'AVNS_xzwmfEDAqtpY_ZcrA1N', [PDO::MYSQL_ATTR_SSL_CA=>true]);
+$stmt = $pdo->query('DESCRIBE interventions');
+while($r=$stmt->fetch()) echo $r['Field'].' | '.$r['Type'].'<br>';
