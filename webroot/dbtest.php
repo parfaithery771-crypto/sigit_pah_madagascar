99affb4db4a53fb1d9e9e4b498cc681f2306c595
parfaithery771-crypto <parfaithery771@gmail.com>
2026-07-01 10:42:46 +0300

    debug columns via app connection

--- webroot/dbtest.php
+++ webroot/dbtest.php
@@ -1,7 +1,14 @@
 ﻿<?php
-$dsn = 'mysql:host=sql7.freesqldatabase.com;port=3306;dbname=sql7830743;charset=utf8mb4';
-$pdo = new PDO($dsn, 'sql7830743', 'X9UVEJYT3f');
-echo "=== LIVRABLES ===<br>";
-foreach($pdo->query('DESCRIBE livrables') as $r) echo $r['Field'].' | '.$r['Type'].'<br>';
+require dirname(__DIR__) . '/vendor/autoload.php';
+require dirname(__DIR__) . '/config/bootstrap.php';
+use Cake\Datasource\ConnectionManager;
+$conn = ConnectionManager::get('default');
 echo "=== INTERVENTIONS ===<br>";
-foreach($pdo->query('DESCRIBE interventions') as $r) echo $r['Field'].' | '.$r['Type'].'<br>';
+$rows = $conn->execute('DESCRIBE interventions')->fetchAll('assoc');
+foreach($rows as $r) echo $r['Field'].' | '.$r['Type'].'<br>';
+echo "<br>=== SAMPLE DATA ===<br>";
+$data = $conn->execute('SELECT * FROM interventions LIMIT 2')->fetchAll('assoc');
+echo '<pre>'; print_r($data); echo '</pre>';
+echo "<br>=== LIVRABLES ===<br>";
+$rows2 = $conn->execute('DESCRIBE livrables')->fetchAll('assoc');
+foreach($rows2 as $r) echo $r['Field'].' | '.$r['Type'].'<br>';
