09ecfb588e14a32cf70899bd3cc214be258df0e9
parfaithery771-crypto <parfaithery771@gmail.com>
2026-07-01 10:16:02 +0300

    check columns livrables interventions

--- webroot/dbtest.php
+++ webroot/dbtest.php
@@ -1,5 +1,7 @@
 ﻿<?php
-$dsn = 'mysql:host=mysql-22e1d2da-sigitmincc.i.aivencloud.com;port=14995;dbname=defaultdb;charset=utf8mb4';
-$pdo = new PDO($dsn, 'avnadmin', 'AVNS_xzwmfEDAqtpY_ZcrA1N', [PDO::MYSQL_ATTR_SSL_CA=>true]);
-$stmt = $pdo->query('DESCRIBE interventions');
-while($r=$stmt->fetch()) echo $r['Field'].' | '.$r['Type'].'<br>';
+$dsn = 'mysql:host=sql7.freesqldatabase.com;port=3306;dbname=sql7830743;charset=utf8mb4';
+$pdo = new PDO($dsn, 'sql7830743', 'X9UVEJYT3f');
+echo "=== LIVRABLES ===<br>";
+foreach($pdo->query('DESCRIBE livrables') as $r) echo $r['Field'].' | '.$r['Type'].'<br>';
+echo "=== INTERVENTIONS ===<br>";
+foreach($pdo->query('DESCRIBE interventions') as $r) echo $r['Field'].' | '.$r['Type'].'<br>';
