1900ab1a283a3649ed27c08b2905aab603642463
parfaithery771-crypto <parfaithery771@gmail.com>
2026-07-01 11:55:24 +0300

    remove debug file

--- webroot/dbtest3.php
+++
@@ -1,7 +0,0 @@
-﻿<?php
-require dirname(__DIR__) . '/vendor/autoload.php';
-require dirname(__DIR__) . '/config/bootstrap.php';
-use Cake\Datasource\ConnectionManager;
-$conn = ConnectionManager::get('default');
-$data = $conn->execute('SELECT id, beneficiaire, statut FROM interventions ORDER BY id')->fetchAll('assoc');
-echo '<pre>'; print_r($data); echo '</pre>';
